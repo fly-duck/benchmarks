61d06d7738b9524f4a8e793df39ff6d0d8f33530
booney <booney@cmf.com>
2019-04-26 20:53:01 +0800

    no builds

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/booney/Myworkspace/sample_ws/src/clear_mot
-# Build directory: /home/booney/Myworkspace/sample_ws/src/clear_mot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

e9318be33794748fe15b7b910aec4263e4a75078
Booney <92931@DESKTOP-357U3SI>
2019-05-05 13:03:48 +0800

    tested the munkres

--- scripts/import_munres.py
+++ scripts/import_munres.py
@@ -1,8 +1,11 @@
 
+# import numpy 
+# a=numpy.array(1)
+# print(a)
+from munkres import Munkres # Third party library. For the minimum matching assignment problem. To install: https://pypi.python.org/pypi/munkres 
 
 import random
-from munkres import Munkres
-
+# from munkres import Munkres
 my_randoms=[]
 for i in range (10):
     my_randoms.append(random.randrange(1,101))
@@ -24,10 +27,9 @@ for i,j in prev_dist2.items() :
 est_people=[]
 gt_people=[]
 est_people=random.sample(range(1,10),8)
-gt_people=random.sample(range(1,10),8)
+gt_people=random.sample(range(2,11),8)
 print(est_people)
 print(gt_people)
-
 
 
 # for i in range(10):
@@ -38,7 +40,7 @@ for est_person in est_people:
     dist_row=[]
     for gt_person in gt_people:
         if est_person not in match and gt_person not in match.values():
-            dist=est_person-gt_person
+            dist=abs(est_person-gt_person)
         else:
             dist=99999
         dist_row.append(dist)
@@ -49,7 +51,11 @@ if dist_matrix:
     munkres=Munkres()
     # munkres2=munkres.Munkres()
     indexes=munkres.compute(dist_matrix)
+    for i , j in indexes:
+        if dist_matrix[i][j]<2:
+            match[est_people[i]]=gt_people[j]
     print(indexes)
+    print (match)
 
 
 
